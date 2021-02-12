32d3e82c8c0db208e3912e0a9db00bb77b43485c
skerakef <ztaouil>
2021-02-12 16:49:12 +0100

    re-up

--- intarray.c
+++ intarray.c
@@ -4,9 +4,14 @@
 typedef struct _intarray
 {
 	int*	tab;
-	int	len;
-}intarray;
+	int		len;
+}				intarray;
 
+intarray	fill_intarray (int *tab, int len);
+intarray	display_intarray (intarray tab);
+void		print_positive_values_intarray (intarray tab);
+int			search_intarray (intarray tab, int n);
+int			nb_occurences_intarray(intarray tab, int n);
 
 intarray	fill_intarray(int *tab, int len)
 {
@@ -16,6 +21,7 @@ intarray	fill_intarray(int *tab, int len)
 	x.len = len;
 	return (x);
 }
+
 intarray	display_intarray(intarray tab)
 {
 	int i;
@@ -25,12 +31,51 @@ intarray	display_intarray(intarray tab)
 	printf("%d]\n", tab.tab[tab.len - 1]);
 }
 
+void		print_positive_values_intarray (intarray tab)
+{
+	int i;
+
+	for (i = 0; i < tab.len ; i++)
+	{
+		if (tab.tab[i] < 0)
+			printf("%d ", tab.tab[i]);
+	}
+}
+
+int			search_intarray (intarray tab, int n)
+{
+	int i;
+
+	for (i = 0; i < tab.len; i++)
+	{
+		if (tab.tab[i] == n)
+			return (1);
+	}
+	return (0);
+}
+
+int			nb_occurences_intarray(intarray tab, int n)
+{
+	int i;
+	int	count;
+
+	count = 0;
+	for (i = 0; i < tab.len; i++)
+	{
+		if (tab.tab[i] == n)
+			count++;
+	}
+	return (count);
+}
+
 int			main()
 {
 	intarray x;
-	int		tab[] = {1, 2, 3, 4};
+	int		tab[] = {1, 2, 10, 2, 10, 10, 10, 2};
+	int		n = 2;
 
-	x = fill_intarray(tab, 4);
+	x = fill_intarray(tab, 8);
 	display_intarray(x);
+	printf("n value : %d\nnb occurences : %d\n", n,nb_occurences_intarray(x, n));
 	return (0);
 }
